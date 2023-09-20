c1d90ddde4ca9cea094a3d3e49cf316b38d34e1e
wonyoungjin <ekszk00@gmail.com>
2023-09-20 10:59:17 +0900

    insert DlgImage

---
+++ gSolution/gPrj/DlgImage.cpp
@@ -0,0 +1,45 @@
+﻿// DlgImage.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "gPrj.h"
+#include "DlgImage.h"
+#include "afxdialogex.h"
+
+
+// CDlgImage 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgImage, CDialogEx)
+
+CDlgImage::CDlgImage(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DlgImage, pParent)
+{
+	m_pParent = pParent;
+}
+
+CDlgImage::~CDlgImage()
+{
+}
+
+void CDlgImage::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgImage, CDialogEx)
+	ON_BN_CLICKED(IDC_BTN_UP_PARENT, &CDlgImage::OnBnClickedBtnUpParent)
+END_MESSAGE_MAP()
+
+
+// CDlgImage 메시지 처리기
+
+#include "gPrjDlg.h"
+void CDlgImage::OnBnClickedBtnUpParent()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	static int n = 100;
+	CgPrjDlg* pDlg = (CgPrjDlg*)m_pParent;
+	pDlg->callFunc(n++);
+	
+}
---
+++ gSolution/gPrj/DlgImage.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+
+	CWnd* m_pParent;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DlgImage };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnUpParent();
+};
